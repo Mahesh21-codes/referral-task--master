106464f4e98822b5dcd45fadb1a24178569b0f43
Mahesh <mahesh959747@gamil.com>
2026-03-19 19:04:14 +0530

    finish

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://referral-task-master-10.onrender.com,
     headers: {
         'Content-Type': 'application/json',
     },
